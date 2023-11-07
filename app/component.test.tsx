b4c817b7e413722596ec4a8cbf7062639e035429
Tony Yunker <tony.yunker@gmail.com>
2023-11-07 11:03:11 -0600

    let's maker it breaker

--- app/component.test.tsx
+++ app/component.test.tsx
@@ -1,12 +1,14 @@
 /**
  * @jest-environment jsdom
  */
-import { fireEvent, render, screen } from '@testing-library/react'
-import Component from './component'
+import { fireEvent, render, screen } from "@testing-library/react";
+import Component from "./component";
+import camelcaseKeys from "camelcase-keys";
 
-it('App Router: Works with Client Components (React State)', () => {
-  render(<Component />)
-  expect(screen.getByRole('heading')).toHaveTextContent('0')
-  fireEvent.click(screen.getByRole('button'))
-  expect(screen.getByRole('heading')).toHaveTextContent('1')
-})
+it("App Router: Works with Client Components (React State)", () => {
+  console.log("👋", camelcaseKeys({ "one-two": 12, "three-four": 34 }));
+  render(<Component />);
+  expect(screen.getByRole("heading")).toHaveTextContent("0");
+  fireEvent.click(screen.getByRole("button"));
+  expect(screen.getByRole("heading")).toHaveTextContent("1");
+});
